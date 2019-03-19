146726b6ffbf75a98156d0e378f1fa2a2f6363de
Tao Lei <whu.eis.lt@gmail.com>
2019-03-18 23:10:58 -0400

    add some tags, add comments

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,11 @@
 const regex = /[^/]+@=[^/]+/g;
 const regexArraySplitter = /[^/]+(?:[^/]*)*/g;
 
+/**
+ * A function to serialize a JSON into string
+ * @param records A JSON object to be serialized to string
+ * @return        STT serialized string
+ */
 const serialize = (records) => {
   if (records === null) {
     return '';
@@ -22,6 +27,11 @@ const serialize = (records) => {
   return serializedString;
 };
 
+/**
+ * A function to deserialized a string into JSON
+ * @param message A STT serialized string, usually received from Douyu danmaku server
+ * @return        STT deserialized JSON representing of the message
+ */
 const deserialize = (message) => {
   if (message === null) {
     return null;
