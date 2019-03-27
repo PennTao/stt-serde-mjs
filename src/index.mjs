ba7a694d06b52b3e0541c784d6bad6d7c5a3d282
Tao Lei <whu.eis.lt@gmail.com>
2019-03-27 00:18:35 -0400

    fix deserializer

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,4 @@
-const regex = /[^/]+@=[^/]+/g;
+const regex = /[^/]+@=[^/]*/g;
 const regexArraySplitter = /[^/]+(?:[^/]*)*/g;
 
 /**
@@ -21,7 +21,7 @@ const serialize = (records) => {
     return records.map(record => serialize(record) + '/').join('');
   }
   Object.entries(records).forEach(([key, value]) => {
-    serializedString = serializedString + key.replace(/@/g, '@A').replace(/\//g, '@S') + '@=' + serialize(value) + '/';
+    serializedString = serializedString + serialize(key) + '@=' + serialize(value) + '/';
   });
 
   return serializedString;
@@ -32,28 +32,26 @@ const serialize = (records) => {
  * @param message A STT serialized string, usually received from Douyu danmaku server
  * @return        STT deserialized JSON representing of the message
  */
+
 const deserialize = (message) => {
   if (message === null) {
     return null;
   }
   const record = {};
-  const items = message.match(regex);
-  if (items === null) {
-    return message;
+  const entries = message.match(regex);
+  const items = message.match(regexArraySplitter);
+  if (entries !== null) {
+    entries.forEach((entry) => {
+      const kvps = entry.split('@=');
+      const key = kvps[0];
+      const value = kvps[1];
+      record[deserialize(key)] = deserialize(value);
+    });
+    return record;
+  } if (items !== null && items.length > 1) {
+    return items.map(item => deserialize(item));
   }
-  items.forEach((item) => {
-    const kvps = item.split('@=');
-    const key = kvps[0];
-    const value = kvps[1];
-    const escapeSlashvalues = value.replace(/@S/g, '/').match(regexArraySplitter);
-    const deserializeValues = escapeSlashvalues.map(escapeSlashvalue => deserialize(escapeSlashvalue.replace(/@A/g, '@').replace(/@S/g, '/').replace(/@A/g, '@')));
-    if (deserializeValues.length > 1) {
-      record[key] = deserializeValues;
-    } else {
-      [record[key]] = deserializeValues;
-    }
-  });
-  return record;
+  return message.replace(/@A/g, '@').replace(/@S/g, '/');
 };
 
 export {
